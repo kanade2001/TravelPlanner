05c5902ce3489366f7b0a4e0eb67665f4cd6e90f
kanade2001 <kanade2001.cpp@gmail.com>
2024-05-21 17:57:32 +0900

    make Register Button

--- frontend/src/routes/Login.jsx
+++ frontend/src/routes/Login.jsx
@@ -1,22 +1,36 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { React, useState } from 'react';
 import '../App.css';
 import TextField from '@mui/material/TextField';
 import Header from '../components/Header.tsx';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Switch from '@mui/material/Switch';
+import { FormControlLabel } from '@mui/material';
 
 function Login() {
-  const JumpLink = (path) => {
-    useNavigate(path);
+  const [isRegister, setIsRegister] = useState(false);
+
+  const handleSwitchChange = (event) => {
+    setIsRegister(event.target.checked);
   }
+
 
   return (
     <div className="App">
       <Header />
-      <Box margin="30px">
-        <TextField id="outlined-basic" label="Mail" valiant="outlined" autoComplete="on" />
-        <TextField id="outlined-basic" label="Password" valiant="outlined" autoComplete="on" />
-        
+      <Box width="80%" margin="auto">
+        <Box margin="30px" display="flex" justifyContent="flex-end">
+          <FormControlLabel control={<Switch checked={isRegister} onChange={handleSwitchChange} />} label="Register" />
+        </Box>
+        <Box marginTop="30px">
+          <TextField fullWidth id="fullWidth" label="Mail" valiant="outlined" autoComplete="on" />
+        </Box>
+        <Box marginTop="30px">
+          <TextField fullWidth id="outlined-basic" label="Password" valiant="outlined" autoComplete="on" />
+        </Box>
+        <Box margin="30px">
+          <Button variant="contained">{isRegister ? "Register" : "Login" }</Button>
+        </Box>
       </Box>
     </div>
   );
